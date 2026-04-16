416ccac6d26dfda50bf7cd1aa9c154f1389f7074
kamalTaha <kamaltaha298@gmail.com>
2026-04-16 14:10:22 +0200

    add custom button in edit notes body view

--- lib/views/widgets/custom_app_bar.dart
+++ lib/views/widgets/custom_app_bar.dart
@@ -1,17 +1,18 @@
 
 import 'package:flutter/material.dart';
-import 'package:notes_app/views/widgets/custom_search_icon.dart';
+import 'package:notes_app/views/widgets/custom_icon.dart';
 
 class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({super.key});
-
+  const CustomAppBar({super.key, required this.text, required this.icon});
+final String text;
+final IconData icon;
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Text('Notes',style: TextStyle(fontSize: 28,color: Colors.white)),
+        Text(text,style: TextStyle(fontSize: 28,color: Colors.white)),
         Spacer(),
-        CustomSearchIcon()
+        CustomIcon(icon: icon,)
       ],
     );
   }
--- lib/views/widgets/edit_note_body_view.dart
+++ lib/views/widgets/edit_note_body_view.dart
@@ -1,10 +1,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/custom_app_bar.dart';
-import 'package:notes_app/views/widgets/notes_list_view.dart';
 
-class NotesViewBody extends StatelessWidget {
-  const NotesViewBody({super.key});
+class EditNoteBodyView extends StatelessWidget {
+  const EditNoteBodyView({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -13,9 +12,7 @@ class NotesViewBody extends StatelessWidget {
       child: Column(
         children: [
           SizedBox(height: 50,),
-          CustomAppBar(text: 'Notes', icon: Icons.search,),
-          SizedBox(height: 20,),
-          Expanded(child: NotesListView())
+          CustomAppBar(text: 'Edit Note', icon: Icons.check,),
         ],
       ),
     );
